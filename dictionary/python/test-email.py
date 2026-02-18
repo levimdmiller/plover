ae86252fb706711babedf596f632a2787dca8223
levimdmiller <levi@levimiller.ca>
2026-02-18 12:32:05 -0700

    update dictionary

--- dictionary/python/test-email.py
+++ dictionary/python/test-email.py
@@ -11,26 +11,22 @@ def lookup(key):
   assert len(key) <= LONGEST_KEY
   if TEST_EMAIL_KEY != key[0]:
     raise KeyError
-  if len(key) > 1:
-    number = key[1].removesuffix('-D')
-    double = key[1].endswith('-D')
-    
 
   emailBase = f"dev+levi.{datetime.today().strftime('%Y-%m-%d')}"
   return f"{emailBase}{get_number(key)}@qhrtest.com"
 
 def get_number(key):
   if len(key) != 2:
-    return '';
-  number = key[1].removesuffix('-D')
+    return ''
+  number = key[1].replace('-', '').removesuffix('D')
 
   if not number.isnumeric():
     raise KeyError
-  
+
   if key[1].endswith('-D'):
     return f'.{number}{number}'
   return f'.{number}'
-  
+
 
 # Optional: return an array of stroke tuples that would translate back
 # to <text> (an empty array if not possible).
